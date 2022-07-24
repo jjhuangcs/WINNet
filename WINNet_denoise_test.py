9ac88244bc1c7c336e1325ed34c472475aa4f391
jjhuangcs <89965355+jjhuangcs@users.noreply.github.com>
2022-07-24 16:12:00 +0800

    Update WINNet_denoise_test.py

--- WINNet_denoise_test.py
+++ WINNet_denoise_test.py
@@ -23,8 +23,6 @@ parser.add_argument("--test_data", type=str, default='Set12', help='test on Set1
 parser.add_argument("--test_noiseL", type=float, default=25, help='noise level used on test set')
 parser.add_argument("--train_noiseL", type=float, default=25, help='noise level used on training set')
 parser.add_argument("--show_results", type=bool, default=False, help="show results")
-parser.add_argument("--start_epoch", type=int, default=1, help="start epoch")
-parser.add_argument("--end_epoch", type=int, default=1, help="end epoch")
 
 opt = parser.parse_args()
 
@@ -42,54 +40,51 @@ def main():
     device_ids = [0]
     model = nn.DataParallel(net, device_ids=device_ids).cuda()
 
-    epoch = opt.start_epoch
-    while epoch <= opt.end_epoch:
-        print('Load model epoch %d' % epoch)
-        model.load_state_dict(torch.load(os.path.join(opt.logdir, 'net_WINNet_epoch_{}.pth'.format(epoch))))
+    print('Load model...')
+    model.load_state_dict(torch.load(os.path.join(opt.logdir, 'net_WINNet.pth')))
 
-        pytorch_total_params = sum(p.numel() for p in model.parameters() if p.requires_grad)
-        print('Total Number of Parameters: ', pytorch_total_params)
+    pytorch_total_params = sum(p.numel() for p in model.parameters() if p.requires_grad)
+    print('Total Number of Parameters: ', pytorch_total_params)
 
-        torch.manual_seed(1234)# for reproducibility
-        model.eval()
-        # load data info
-        files_source = glob.glob(os.path.join('data', opt.test_data, '*.png'))
-        files_source.sort()
-        # process data
-        psnr_avg = 0
-        i = 1
-        for f in files_source:
-            # image
-            Img = cv2.imread(f)
-            Img = normalize(np.float32(Img[:,:,0]))
-            Img = np.expand_dims(Img, 0)
-            Img = np.expand_dims(Img, 1)
-            ISource = torch.Tensor(Img)
-            # noise
-            noise = torch.FloatTensor(ISource.size()).normal_(mean=0, std=opt.test_noiseL/255.)
-            # noisy image
-            INoisy = ISource + noise
-            ISource, INoisy = Variable(ISource.cuda()), Variable(INoisy.cuda())
+    torch.manual_seed(1234)# for reproducibility
+    model.eval()
+    # load data info
+    files_source = glob.glob(os.path.join('data', opt.test_data, '*.png'))
+    files_source.sort()
+    # process data
+    psnr_avg = 0
+    i = 1
+    for f in files_source:
+        # image
+        Img = cv2.imread(f)
+        Img = normalize(np.float32(Img[:,:,0]))
+        Img = np.expand_dims(Img, 0)
+        Img = np.expand_dims(Img, 1)
+        ISource = torch.Tensor(Img)
+        # noise
+        noise = torch.FloatTensor(ISource.size()).normal_(mean=0, std=opt.test_noiseL/255.)
+        # noisy image
+        INoisy = ISource + noise
+        ISource, INoisy = Variable(ISource.cuda()), Variable(INoisy.cuda())
 
-            stdNv_test = Variable(opt.test_noiseL * torch.ones(1).cuda())
-            stdNv_train = Variable(opt.train_noiseL * torch.ones(1).cuda())
-            with torch.no_grad():
-                Out, _ = model(INoisy, stdNv_test, stdNv_train)
-                Out = torch.clamp(Out, 0., 1.)
-            psnr = batch_PSNR(Out, ISource, data_range=1., crop=0)
-            
-            # save results
-            if opt.show_results:
-                save_out_path = "results/WINNet/epoch_{}_img_{}.png".format(epoch, i)
-                save_img(save_out_path, Out)
-                save_out_path = "results/Noisy/nimg_{}.png".format(i)
-                save_img(save_out_path, torch.clamp(INoisy, 0., 1.))
-            print("%s PSNR %f" % (f, psnr))
-            i += 1
-            psnr_avg += psnr
-        psnr_avg /= len(files_source)
-        print("Average PSNR: %f" % (psnr_avg))
-        epoch += 1
+        stdNv_test = Variable(opt.test_noiseL * torch.ones(1).cuda())
+        stdNv_train = Variable(opt.train_noiseL * torch.ones(1).cuda())
+        with torch.no_grad():
+            Out, _ = model(INoisy, stdNv_test, stdNv_train)
+            Out = torch.clamp(Out, 0., 1.)
+        psnr = batch_PSNR(Out, ISource, data_range=1., crop=0)
+
+        # save results
+        if opt.show_results:
+            save_out_path = "results/WINNet/img_{}.png".format(i)
+            save_img(save_out_path, Out)
+            save_out_path = "results/Noisy/nimg_{}.png".format(i)
+            save_img(save_out_path, torch.clamp(INoisy, 0., 1.))
+        print("%s PSNR %f" % (f, psnr))
+        i += 1
+        psnr_avg += psnr
+    psnr_avg /= len(files_source)
+    print("Average PSNR: %f" % (psnr_avg))
 
 if __name__ == "__main__":
     main()
